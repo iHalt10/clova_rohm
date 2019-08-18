341c66b06459f63e67c815eb34740ef4994cc73a
Takeaki Oura <rtm6@me.com>
2019-08-18 18:30:45 +0900

    7commit

--- app.js
+++ app.js
@@ -1,79 +1,20 @@
-const clova = require('@line/clova-cek-sdk-nodejs');
-const express = require('express');
-const logger = require('heroku-logger');
-const app = express();
-const port = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 const io = require('socket.io')(http);
+const PORT = process.env.PORT || 7000;
 
-const clovaSkillHandler = clova.Client
-    .configureSkill()
-
-    //起動時に喋る
-    .onLaunchRequest(responseHelper => {
-        responseHelper.setSimpleSpeech({
-            lang: 'ja',
-            type: 'PlainText',
-            value: '形にしたい言葉を言ってください',
-        });
-    })
-
-    //ユーザーからの発話が来たら反応する箇所
-    .onIntentRequest(async responseHelper => {
-    const intent = responseHelper.getIntentName();
-    const sessionId = responseHelper.getSessionId();
-        // console.log('dddd');
-    // console.log('Intent:' + intent);
-    logger.info('UserId', { sessionId: responseHelper.getUser().userId});
-    if(intent === 'SpeakIntent'){
-        const slots = responseHelper.getSlots();
-        // console.log(slots);
-        //デフォルトのスピーチ内容を記載 - 該当スロットがない場合をデフォルト設定
-        let speech = {
-            lang: 'ja',
-            type: 'PlainText',
-            value: `すみません、聞き取れませんでした。`
-        }
-        var kind = -1;
-        if (slots.Speak_Type === 'がんばれ'){
-            speech.value = `テスト、がんばれ`;
-            kind = 1;
-        } else if (slots.Speak_Type === 'かわいい'){
-            speech.value = `テスト、かわいい`;
-            kind = 2;
-        }
-        else if (slots.Speak_Type === 'かっこいい'){
-            speech.value = `テスト、かっこいい`;
-            kind = 3;
-        }
-        else if (slots.Speak_Type === 'すき') {
-            speech.value = `テスト、すき`;
-            kind = 4;
-        }
-        else if (slots.Speak_Type === 'びっくり') {
-            speech.value = `テスト、びっくり`;
-            kind = 5;
-        }
-        io.emit('word', {
-            str: kind
-        });
-        responseHelper.setSimpleSpeech(speech);
-        responseHelper.setSimpleSpeech(speech, true);
-    }
-})
-
-    //終了時
-    .onSessionEndedRequest(responseHelper => {
-        const sessionId = responseHelper.getSessionId();
-    })
-    .handle();
-
-//リクエストの検証を行う場合。環境変数APPLICATION_ID(値はClova Developer Center上で入力したExtension ID)が必須
-const clovaMiddleware = clova.Middleware({applicationId: 'com.rohm.takeaki'});
-app.post('/clova', clovaMiddleware, clovaSkillHandler);
-
-io.on('connection', function (socket) {
-    logger.info('Connected');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(port, () => console.log(`Server running on ${port}`));
+io.on('connection', function (socket) {
+    socket.on('message', function (msg) {
+        console.log('message: ' + msg);
+        io.emit('message', msg);
+    });
+});
+
+http.listen(PORT, function () {
+    console.log('server listening. Port:' + PORT);
+});
